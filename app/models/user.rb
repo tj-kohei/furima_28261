796a7ab5271cb0ebedd993f1082a14d904bef76e
tj-kohei <68324989+tj-kohei@users.noreply.github.com>
2020-08-11 15:31:03 +0900

    データ管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 end
