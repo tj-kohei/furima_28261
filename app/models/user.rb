85569d60bcd8174a57bc548780eb245ffd832d59
tj-kohei <68324989+tj-kohei@users.noreply.github.com>
2020-08-11 15:49:07 +0900

    ユーザー管理機能修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+with_options presence: true do
+  validates :nickname,
+  validates :email, 
+  validates :password, 
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday, 
+end
 end
