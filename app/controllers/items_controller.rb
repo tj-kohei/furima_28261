4d1d7a8ece9509103a7c6b1c35ab42a421f30283
tj-kohei <68324989+tj-kohei@users.noreply.github.com>
2020-08-20 17:44:46 +0900

    gem payjpの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :owner?, only: [:edit, :update, :delete]
+  
   def index
     @items = Item.order("created_at DESC")
   end
@@ -21,10 +23,34 @@ class ItemsController < ApplicationController
     end
   end
 
+    def edit
+    end
+
+    def update
+      if @item.update(item_params)
+        redirect_to item_path(@item)
+      else
+        render :edit
+    end
+  end
+    def destroy
+      if @item.destroy
+        redirect_to items_path
+      else
+        render item_path(@item)
+    end
+  end
+
   private
 
   def set_item
     @item = Item.find_by(id: params[:id])
+  end
+  
+  def owner?
+    if @item.user != current_user
+      redirect_to items_path
+    end
   end
 
   def item_params
